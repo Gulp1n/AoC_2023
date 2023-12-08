a89ea890cc228a18535e4accbd8100e248b6e5ad
gulp1n <abeldenouden@hotmail.com>
2023-12-08 16:53:28 +0100

    part 2 done?

--- AoC_day8/src/main/kotlin/Main.kt
+++ AoC_day8/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    partOne()
+    partTwo()
 }
 
 fun partOne() {
@@ -16,13 +16,44 @@ fun partOne() {
             location = getNextLocation(location, direction, data)
             steps += 1
             if (location.name == "ZZZ") {
-                println(steps)
                 searching = false
             }
         }
     }
 
     println(steps)
+}
+
+fun partTwo() {
+    val directions = directionData
+    val data = getData()
+
+    var locationList = getStartingLocations(data)
+
+    var searching = true
+    var steps = 0
+
+    while (searching) {
+        directions.forEach { direction ->
+            locationList = getAllNextLocations(locationList, direction, data)
+            steps += 1
+            if (endReached(locationList)) {
+                searching = false
+            }
+        }
+    }
+
+    println(steps)
+}
+
+fun getAllNextLocations(locationList: List<MapElement>, direction: Char, data: List<MapElement>): List<MapElement> {
+    val list: MutableList<MapElement> = mutableListOf()
+
+    locationList.forEach { location ->
+        list.add(getNextLocation(location, direction, data))
+    }
+
+    return list.toList()
 }
 
 fun getNextLocation(location: MapElement, direction: Char, data: List<MapElement>): MapElement {
@@ -35,4 +66,26 @@ fun getNextLocation(location: MapElement, direction: Char, data: List<MapElement
         return res
     }
     TODO()
+}
+
+fun getStartingLocations(data: List<MapElement>): List<MapElement> {
+    val startList: MutableList<MapElement> = mutableListOf()
+
+    data.forEach { step ->
+        if (step.name.last() == 'A') {
+            startList.add(step)
+        }
+    }
+
+    return startList.toList()
+}
+
+fun endReached(locations: List<MapElement>): Boolean {
+    locations.forEach { location ->
+        if (location.name.last() != 'Z') {
+            return false
+        }
+    }
+
+    return true
 }
